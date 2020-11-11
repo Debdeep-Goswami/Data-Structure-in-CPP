befadcfb92b412e4b919d8ae61d68cdc2363c956
Debdeep Goswami <53962338+Debdeep-Goswami@users.noreply.github.com>
2020-11-11 23:48:02 +0530

    Add files via upload

---
+++ Queue using Array/main.cpp
@@ -0,0 +1,46 @@
+/**********************************************
+        Queue using Array
+***********************************************/
+
+#include <iostream>
+#include <cstdlib>
+#define MAX 1000
+
+using namespace std;
+
+class Queue
+{
+
+private :
+    int top;
+    int queue_size;
+
+public:
+    int data_array[MAX];
+
+    //  Constructor
+    Queue(){top=-1;}
+
+    //--------------------- Necessary for Stack Operation -------
+    void enqueue(int);
+    int dequeue();
+
+    // -------------------- Other utility functions --------------
+    void display_queue();
+    int menu();
+    void run();
+};
+
+
+//_________________________ Main Function ________________
+
+int main()
+{
+    Queue q;
+    q.run();
+    cout <<endl;
+    return 0;
+}
+// _______________________ END of Main Function __________
+
+
